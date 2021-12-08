35fdd3ac3b680f141fe293e97d69b588b61bcd8b
Adriana <am20dipi@alum.siena.edu>
2021-12-08 14:43:06 -0500

    adding coding examples; arithmetic operators to numbers.js

---
+++ numbers.js
@@ -0,0 +1,31 @@
+// NUMBERS
+
+let radius = 10
+const pi = 3.14
+
+// arithmetic operators +, -, *, /, **, %
+
+    // JS follows order of operations: PEMDAS lol
+    
+    let result = 5 * (10-3)**2 // => 245
+    console.log(result)
+
+
+// incrementing & decrementing + shorthand notation
+
+let likes = 10
+console.log(likes++) // => 11
+console.log(likes--) // => 9
+console.log(likes+=10) // => 20
+console.log(likes-=5) // => 5
+console.log(likes*=2) // => 20
+
+
+// NaN -- not a number
+    // we receive this when something is not a number
+
+
+// Concatenating Numbers
+
+let concat = "My blog has " +  likes + ' likes.'
+console.log(concat)
