451799406ed638eda1181ffce83d014dcdeac08e
Adriana <am20dipi@alum.siena.edu>
2021-12-08 14:27:06 -0500

    Coding out common string methods with examples; creating numbers.js

--- strings.js
+++ strings.js
@@ -27,7 +27,7 @@ console.log(fullName.length)
 
 
 
-// strings have methods
+// strings have METHODS
     // a method is a function associated with a specific data type
 
 console.log(fullName.toUpperCase()) // => 'ADRIANA D.'
@@ -37,3 +37,20 @@ console.log(fullName.toLowerCase()) // => 'adriana d.'
     // but some methods do.
 
 console.log(fullName.indexOf('d')) // => 1
+
+
+let email = 'adriana@gmail.com'
+
+let slicedResult = email.slice(0, 4) // => 'adri'
+// slice: first arg is start point, second arg is end point
+// so our output shows characters from index of 0 -> 4
+
+let substringResult = email.substr(1, 3) // => 'dri'
+// substr: first arg is start point, second arg is how many characters we want to retrieve
+// so our output shows 3 characters starting from index of 1 
+
+let replacedResult = email.replace('d', 'x') // => 'axriana@gmail.com'
+// replace: replaces first arg with second arg; ONLY replaces first character; NOT REPETITIVE
+
+
+
