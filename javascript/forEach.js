d9df55e0f79ec2b217ab1f3be78046b0f6b506dd
Adriana <am20dipi@alum.siena.edu>
2021-12-14 11:48:10 -0500

    Creating forEach.js file; coding out a simple callback function and .forEach() method to show the use of callbacks; adding notes

---
+++ javascript/forEach.js
@@ -0,0 +1,24 @@
+// callback functions using .forEach()
+
+// declaring + assiging 'fruit' to an array of strings
+const fruit = ['cherry', 'mango', 'pear']
+
+// creating an arrow function that takes in a fruit and an index
+// console logging the a string literal with the index followed by the fruit
+
+const logFruit = (fruit, index) => {
+    console.log(`${index} -- ${fruit}`)
+}
+
+// instead of looping through the array of fruit within the function
+// we callback logFruit by passing it to the .forEach() method
+
+fruit.forEach(logFruit)
+
+// our output:
+    // 0 -- cherry
+    // 1 -- mango
+    // 2 -- pear
+
+
+// callback functions -- functions we pass to another function (or method) as an argument
--- notes/functions_methods.md
+++ notes/functions_methods.md
@@ -13,3 +13,10 @@ So, methods are functions, but they are defined on an object or data type.
 
 
 The difference lies in where we call them and where they are defined. 
+
+
+## CALLBACKS
+
+Functions that are invoked at a later moment in time.
+
+Functions are first class data -- meaning functions can be passed in as an argument. 
