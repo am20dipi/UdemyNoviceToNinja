d989d11be0e7996f53115523c16fb68c339e5abf
Adriana <am20dipi@alum.siena.edu>
2021-12-23 11:57:06 -0500

    Buidling out example to use regex on a form event listener

--- javascript/regex.js
+++ javascript/regex.js
@@ -16,8 +16,30 @@
     // [a-z] only allows for a single char between a and z
     // [a-z A-Z] only allows for a single char uppercase or lowercase
     // [a-z A-Z](6,10) only allows for input between 6 and 10 chars long
+    // [a-z 0-9] allows for nums + letters
 
 // g
     // global modifier -- all matches?
 
+// .
+    // . before length requirement allows for ANY character including symbols, punctuation...
+    // /^.(5, 12)$
 
+
+
+
+// EXAMPLE
+
+const form = document.querySelector('.signup-form')
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    console.log(form.username.value)
+})
+
+const username = 'shaun'
+const pattern = /[a-z]{6,}/
+// any lowercase letters, min of 6 chars long -- no max. 
+
+let result = pattern.test(username)
+// if passes the regex test, returns true; otherwise returns false
