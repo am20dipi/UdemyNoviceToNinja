84097595f9a4bef79b771757135109611289291e
Adriana <am20dipi@alum.siena.edu>
2021-12-23 11:52:55 -0500

    Creating file regex.js; adding notes on different regex notations

---
+++ javascript/regex.js
@@ -0,0 +1,23 @@
+// REGEX
+    // regular expressions
+    // matching patterns -- used to check if input matches requirements
+    // regexes can tell us how long an input should be, what characters are required, what characters are not allowed...
+    // used often for validation, passwords, email signup
+
+// $
+    // nothing after it
+    // asserts position at end of the line
+
+// ^ 
+   // nothing before it
+   // asserts position at start of a line
+
+// character set []
+    // [a-z] only allows for a single char between a and z
+    // [a-z A-Z] only allows for a single char uppercase or lowercase
+    // [a-z A-Z](6,10) only allows for input between 6 and 10 chars long
+
+// g
+    // global modifier -- all matches?
+
+
