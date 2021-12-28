efe6745e464b78e466be7147bacc40d97f305c06
Adriana <am20dipi@alum.siena.edu>
2021-12-28 15:03:15 -0500

    Creating function tick() that retrieves the current date by instantiating new Date object; retrieving hour, minutes and seconds from current date; appending the current hour, mins and seconds to the page using template literal; setting an interval to continue counting the time

---
+++ mini projects/digital clock/clock.js
@@ -0,0 +1,17 @@
+const clock = document.querySelector('.clock')
+
+const tick = () => {
+    const now = new Date()
+    const hour = now.getHours()
+    const minutes = now.getMinutes()
+    const seconds = now.getSeconds()
+
+    //console.log(hour, minutes, seconds)
+
+    const html = `
+        <span>${hour}:${minutes}:${seconds}</span>
+    `
+    clock.innerHTML = html
+}
+
+setInterval(tick, 1000)
