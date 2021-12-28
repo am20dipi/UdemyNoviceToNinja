bba304c58ae3beb0f257fbaad8d4a77cd13590bd
Adriana <am20dipi@alum.siena.edu>
2021-12-28 11:50:02 -0500

    Building out example using map() and writing some notes on map()

--- javascript/arrayMethods.js
+++ javascript/arrayMethods.js
@@ -8,9 +8,26 @@
     // filter() is NON-DESTRUCTIVE -- so we must store .filter() in a variable
 
     const scores = [10, 20, 30, 40, 50]
-    
+
     const filteredScores = scores.filter(() => {
         return score > 20
     })
     console.log(filteredScores)
     // returns [30, 40, 50]
+
+
+// MAP()
+
+    // takes an array and iterates through it
+    // fires a callback function for each item in the array
+    // creates an updated value for each array item and pushes it in a new array
+    // returns the new array
+    // map() is NON-DESTRUCTIVE -- so we must store it in a variable
+
+    const prices = [20, 25, 27, 44, 54]
+
+    const salePrices = prices.map((price) => {
+        return price * 0.50
+    })
+    console.log(salePrices)
+    
