8951a300f8e26254f29803641825bc85436eb911
Adriana <am20dipi@alum.siena.edu>
2021-12-29 17:41:51 -0500

    Creating asyncAwait.js; building out asyn function getMovies(); using await keyword to stall JavaScript

---
+++ javascript/asyncAwait.js
@@ -0,0 +1,21 @@
+// ASYNC + AWAIT
+
+// both allow us to chain Promises ...
+// ... together in a more clean and readable way
+
+// async function
+    // everytime we call async function, we are returned a Promise!
+    // async functions are NON-BLOCKING
+const getMovies = async () => {
+
+    // await keyword stalls JS -- it stops it from assigned a value to the response variable
+    // until the Promise has resolved
+    const response = await fetch('http://example.com/movies.json')
+    console.log(response)
+
+    // since .json() returns a Promise, we need to use await again
+    const data = await response.json()
+    console.log(data)
+}
+
+getMovies()
