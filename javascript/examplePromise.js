64e0a723e0a4d5469de9da88bf0f5519675f072e
Adriana <am20dipi@alum.siena.edu>
2021-12-29 16:59:33 -0500

    Adding in line comments and notes to examplePromise.js

--- javascript/examplePromise.js
+++ javascript/examplePromise.js
@@ -9,20 +9,30 @@ const getTodos = (resource) => {
         // user event === readystatechange
         request.addEventListener('readystatechange', () => {
             if (request.readyState === 4 && request.status === 200){
+                // parsing data from JSON string to JS object
                 const data = JSON.parse(request.responseText)
                 resolve(data)
             } else if (request.readyState === 4){
                 reject('error')
             }
         })
-
+        // initializing request; params: tells browser to retrieve (GET) data from specified resource
         request.open('GET', resource)
+        // sending the request to the server
         request.send()
     })
 }
 
+// if request is resolved, the .then() method is called
+// if request is rejected, the .catch() method is called
 getTodos().then(data => {
     console.log('promise resolved: ', data)
 }).catch(err => {
     console.log(`promise rejected: `, err)
 })
+
+
+// The XMLHttpRequest (XHR) object is used to interact with servers. 
+// You can retrieve data from a URL without having to do a full 
+// page refresh. This enables a Web page to update just part of a 
+// page without disrupting what the user is doing.
