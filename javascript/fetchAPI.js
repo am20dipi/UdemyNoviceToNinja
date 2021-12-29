a15393f9fc661697881172e70e18dda2cb39ab18
Adriana <am20dipi@alum.siena.edu>
2021-12-29 17:17:54 -0500

    parsing response using .json(); chaining another then() method to the parsed response and logging the data to the console; adding in line comments

--- javascript/fetchAPI.js
+++ javascript/fetchAPI.js
@@ -7,9 +7,18 @@
 // these Promise methods can be chained together
 
 fetch('http://example.com/movies.json')
+
    // for resolved
     .then((response) => {
         console.log(response)
+        // .json() parses the response data for us
+        // this will return a PROMISE!
+        return response.json()
+    })
+    // chain another .then() to response.json() since it returns a Promise
+    // pass in the data and paste it to the console
+    .then((data) => {
+        console.log(data)
     })
     // for rejected
     .catch((err) => {
