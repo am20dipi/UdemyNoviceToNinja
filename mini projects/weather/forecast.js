4e0f678eb05333e6d59c85b97ba5ce006cc2f061
Adriana <am20dipi@alum.siena.edu>
2021-12-29 19:30:08 -0500

    Building out getWeather() with an async function and using fetch(); chaining then() methods to return the data of a specific city

--- mini projects/weather/forecast.js
+++ mini projects/weather/forecast.js
@@ -1,8 +1,19 @@
 const key = '7P9yAWDrDjtxBb3Oj1mOAYojpTKURpx2'
 
+// get weather info
+
+const getWeather = async (id) => {
+    
+    const endpoint = 'http://dataservice.accuweather.com/currentconditions/v1/'
+    const query = `${id}?apikey=${key}`
+
+    const response = await fetch(endpoint + query)
+    const data = await response.json()
+    return data[0]
+}
 
 
-
+// get city key
 const getCity = async (city) => {
     
     const endpoint = 'http://dataservice.accuweather.com/locations/v1/cities/search'
@@ -10,8 +21,17 @@ const getCity = async (city) => {
 
     const response = await fetch(endpoint + query)
     const data = await response.json()
-    console.log(data[0])
+    return data[0]
 
 }
 
 getCity('manchester')
+    .then((data) => {
+        return getWeather(data.Key)
+    }).then((data) => {
+        console.log(data)
+    })
+    .catch((error) => {
+        console.log(error.message)
+    })
+
