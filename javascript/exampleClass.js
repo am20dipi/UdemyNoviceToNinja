3ae7eaf6a923474de94fc381932e6c08ee180055
Adriana <am20dipi@alum.siena.edu>
2022-01-01 13:45:55 -0500

    Adding another method into user class; learning to chain methods to an instance of user class by returning 'this'

--- javascript/exampleClass.js
+++ javascript/exampleClass.js
@@ -4,12 +4,18 @@ class User {
     constructor(username, email){
         this.username = username
         this.email = email
+        this.score = 0
     }
     login(){
         console.log(`${this.username} just logged in.`)
     }
     logout(){
         console.log(`${this.username} just logged out.`)
+    }
+    increaseScore(){
+        this.score++
+        console.log(`${this.score}`)
+        return this
     }
 }
 
@@ -19,3 +25,11 @@ const userTwo = new User('dillon', 'dillon@gmail.com')
 console.log(userOne, userTwo)
 userOne.login()
 userTwo.login()
+
+
+// HOW TO CHAIN METHODS
+
+// in order to chain methods, you must return 'this' within the method!!
+// returning 'this' is returning the instance of the user object the methods are called on. 
+
+
